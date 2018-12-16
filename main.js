ffc0737646f3f9e15df17b7628323edda2a6af40
Rarpainting <zhandequan@yahoo.com>
2018-12-16 21:37:20 +0800

    update

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const {app,BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const url = require('url')
 const path = require('path')
+const { exec } = require('child_process')
 
 let mainWindow = null
+let workProcess = exec() // 后台 go 进程
 
 app.on('ready',() => {
+	workProcess = exec("./backstage/gocmd")
 	mainWindow = new BrowserWindow({
 		width:1200,
 		height:800
 	})
+	// 有问题 不应该是 file: 而是 https:
 	mainWindow.loadURL(url.format({
-		pathname:path.join(__dirname,'/app/index.html'),
-		protocol:'file:',
+		// pathname:path.join(__dirname,'/app/index.html'),
+		// protocol:'file:',
 		slashes:true
 	}))
 })
@@ -22,6 +26,7 @@ app.on('close', () => {
 
 app.on('window-all-closed', () => {
 	if (process.platform != 'darwin') {
+		workProcess.kill()
 		app.quit()
 	}
 })
